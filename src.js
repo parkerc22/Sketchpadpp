8509dc7ba12f515b24bec12dfc561de07a7ca0f1
parkerc22 <parkerc@princeton.edu>
2025-03-25 13:52:36 -0400

    testing

--- src.js
+++ src.js
@@ -3,21 +3,7 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
     canvas.addEventListener("click", function(event) {
-      window.alert("ATTENTION\n thank you for your attention");
-
       const x = event.offsetX;
       const y = event.offsetY;
-      const size = 20; // Shape size
-      const shapeType = "circle"; // or "rectangle"
-
-      if (shapeType === "circle") {
-        ctx.beginPath();
-        ctx.arc(x, y, size / 2, 0, 2 * Math.PI);
-        ctx.fillStyle = "red";
-        ctx.fill();
-        ctx.closePath();
-      } else if (shapeType === "rectangle") {
-         ctx.fillStyle = "blue";
-         ctx.fillRect(x - size / 2, y - size / 2, size, size);
-      }
+      window.alert("wow! you clicked at x=" + x + " and y=" + y);
     });
