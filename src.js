31252c1117c21c737d411e54df102eac27ef6700
parkerc22 <parkerc@princeton.edu>
2025-03-30 14:08:04 -0400

    testing move

--- src.js
+++ src.js
@@ -1,9 +1,18 @@
 window.alert("click in the box");
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-
+isDrawing = false;
+//assume just line tool for now
     canvas.addEventListener("click", function(event) {
       const x = event.offsetX;
       const y = event.offsetY;
-      window.alert("wow! you clicked at x=" + x + " and y=" + y);
+      ctx.beginPath();
+      ctx.moveTo(x,y);
+      isDrawing = !isDrawing;
+    });
+    canvas.addEventListener("mousemove", function(event) {
+      window.alert("diddy alert");
+
+
+
     });
