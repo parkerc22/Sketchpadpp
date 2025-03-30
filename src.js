1fb7391cf3ea7fae61ee26032694a46b7f3204e9
parkerc22 <parkerc@princeton.edu>
2025-03-30 18:42:35 -0400

    added zoom in and out

--- src.js
+++ src.js
@@ -1,3 +1,10 @@
+//-----TO-DO----------------------
+//Add CCW vs CW circle functionality
+//create pixel transform (zoom in/out)
+//snap to endpoints
+//create ability to link
+//create ability to move
+
 window.alert("click in the box");
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
@@ -5,6 +12,8 @@ canvas.focus();
 isDrawing = false;
 drawCircle = true;
 drawLine = false;
+//the coordinate transformation to map from coordinates to pixels (scale and then offset)
+windowTransform = {Xscale: 1.0, Yscale: 1.0, Xoffset: 0.0, Yoffset: 0.0}
 //line by default
 
 //----------TOOLS-----------------------------------------------------
@@ -13,6 +22,36 @@ function dist(x1, y1, x2, y2) {
   return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
 
+function coordToPix(x, y, transform) {
+  return [(x*transform.Xscale) + transform.Xoffset, (y*transform.Yscale) + transform.Yoffset];
+}
+
+function pixToCoord(x, y, transform) {
+  return [(x-transform.Xoffset) / transform.Xscale, (y-transform.Yoffset) / transform.Yscale];
+
+}
+
+function distToCoord(dist, transform) {
+  return dist / transform.Xscale;
+}
+
+function distToPix(dist, transform) {
+  return dist * transform.Xscale;
+}
+
+function drawDisplay(ctx, canvas, lines, circles) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (i = 0; i < lines.length; i++) {
+      if (lines[i].isFinished) {
+        lines[i].draw(ctx);
+      }
+    }
+    for (c = 0; c < circles.length; c++) {
+      if(circles[c].isFinished) {
+        circles[c].draw(ctx);
+      }
+    }
+}
 //---------OBJECTS-------------------------------------------------------
 
 class Line {
@@ -25,9 +64,11 @@ class Line {
   }
   draw(ctx) {
     // Start a new Path
+    var pix1 = coordToPix(this.x1, this.y1, windowTransform);
+    var pix2 = coordToPix(this.x2, this.y2, windowTransform);
     ctx.beginPath();
-    ctx.moveTo(this.x1, this.y1);
-    ctx.lineTo(this.x2, this.y2);
+    ctx.moveTo(pix1[0], pix1[1]);
+    ctx.lineTo(pix2[0], pix2[1]);
 
     // Draw the Path
     ctx.stroke();
@@ -45,15 +86,19 @@ class Circle {
   }
   draw(ctx) {
     // Start a new Path
+    var pix = coordToPix(this.x, this.y, windowTransform);
+    var _x = pix[0];
+    var _y = pix[1];
+    var _r = distToPix(this.r, windowTransform);
     if (this.radSet) {
       ctx.beginPath();
-      ctx.arc(this.x, this.y, this.r, this.d1, this.d2);
+      ctx.arc(_x, _y, _r, this.d1, this.d2);
 
       // Draw the Path
       ctx.stroke();
     } else {
       //if the radius is not yet set, just draw a point where the center was chosen
-      ctx.fillRect(this.x-1,this.y-1,3,3)
+      ctx.fillRect(_x-1,_y-1,3,3)
     }
   }
 }
@@ -63,10 +108,10 @@ circles = [];
 
 //assume just line tool for now
 canvas.addEventListener("click", function(event) {
-  const x = event.offsetX;
-  const y = event.offsetY;
-  ctx.beginPath();
-  ctx.moveTo(x,y);
+  const coord = pixToCoord(event.offsetX, event.offsetY, windowTransform);
+  x = coord[0];
+  y = coord[1];
+
   if (drawLine && !isDrawing) {
     l = new Line(x,y,x,y);
     lines.push(l);
@@ -105,31 +150,23 @@ canvas.addEventListener("click", function(event) {
 });
 
 canvas.addEventListener("mousemove", function(event) {
-  
+  const coord = pixToCoord(event.offsetX, event.offsetY, windowTransform);
+  x = coord[0];
+  y = coord[1];
   if (isDrawing) {
     
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (i = 0; i < lines.length; i++) {
-      if (lines[i].isFinished) {
-        lines[i].draw(ctx);
-      }
-    }
-    for (c = 0; c < circles.length; c++) {
-      if(circles[c].isFinished) {
-        circles[c].draw(ctx);
-      }
-    }
+    drawDisplay(ctx, canvas, lines, circles);
     if (drawLine) {
       l = lines[lines.length-1];
-      l.x2 = event.offsetX;
-      l.y2 = event.offsetY;
+      l.x2 = x;
+      l.y2 = y;
       l.draw(ctx);
     } else if (drawCircle) {
       c = circles[circles.length-1];
       if (c.radSet) {
         //if radius is set, then move around the arc lengths
-        c.d2 = Math.atan((event.offsetY-c.y)/(event.offsetX-c.x));
-        if (event.offsetX-c.x < 0) {
+        c.d2 = Math.atan((y-c.y)/(x-c.x));
+        if (x-c.x < 0) {
           c.d2 += Math.PI;
         }
       }
@@ -174,5 +211,17 @@ canvas.addEventListener("keydown", function(event) {
         circles.pop();
       }
     }
+  }
+  else if (key === "a"){
+    //zoom in
+    windowTransform.Xscale *= 1.1;
+    windowTransform.Yscale *= 1.1;
+    drawDisplay(ctx, canvas, lines, circles);
+  }
+  else if (key === "s") {
+    //zoom out
+    windowTransform.Xscale /= 1.1;
+    windowTransform.Yscale /= 1.1;
+    drawDisplay(ctx, canvas, lines, circles);
   }
 });
